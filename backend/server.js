822d19160348f85ca0b114340fac45610f489738
Harshit Rishav <128921989+harshitrishav2003@users.noreply.github.com>
2025-01-02 16:35:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5173',
+    origin: 'https://design-your-career-path-hvc5.vercel.app/',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
