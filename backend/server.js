34863b534a048038f0b67666ee9560efb46042f4
Harshit Rishav <harshitrishav987@gmail.com>
2025-01-30 01:54:11 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const { exec } = require('child_process'); // To run the LaTeX compiler
 const { PDFDocument, StandardFonts } = require('pdf-lib');
 const cors = require('cors');
-
+const port = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
 app.use(cors({
@@ -503,7 +503,7 @@ ${customSections}
 });
 
 app.use('/resumes', express.static(path.join(__dirname, 'resumes')));
-const port = process.env.PORT || 5001;
-app.listen(5001, () => {
-    console.log('Server running on http://localhost:5001');
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
